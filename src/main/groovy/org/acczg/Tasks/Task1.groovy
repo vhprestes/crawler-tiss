b356df443c96f269808d6c4613c554116ca67a63
vhprestes <vhprestes@gmail.com>
2024-10-08 20:15:15 -0300

    feat: task 1 complete

---
+++ src/main/groovy/org/acczg/Tasks/Task1.groovy
@@ -0,0 +1,33 @@
+package org.acczg.Tasks
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+class Task1 {
+
+    static def run(baseUrl) {
+
+            try {
+                Document doc = Jsoup.connect(baseUrl).get()
+
+                String firstLink = doc.select("ul.list-navigation:nth-child(2) > li:nth-child(1) > ul:nth-child(2) > li:nth-child(6) > a:nth-child(1)").attr("href")
+                doc = Jsoup.connect(firstLink).get()
+
+                String secondLink = doc.select("div.card:nth-child(1) > a:nth-child(1)").attr("href")
+                doc = Jsoup.connect(secondLink).get()
+
+                String thirdLink = doc.select("p.callout:nth-child(4) > a:nth-child(1)").attr("href")
+                doc = Jsoup.connect(thirdLink).get()
+
+
+                String downloadLink = doc.select(".table > tbody:nth-child(2) > tr:nth-child(5) > td:nth-child(3) > a:nth-child(1)").attr("href")
+
+                def fileResponse = Jsoup.connect(downloadLink).ignoreContentType(true).execute()
+                new File("../../downloads/arquivo_padrao_tiss.zip").withOutputStream { it << fileResponse.bodyStream() }
+                print("Arquivo baixado com sucesso")
+            }
+            catch (Exception e) {
+                println "Erro ao baixar arquivo padrão TISS" + e
+            }
+        }
+    }
