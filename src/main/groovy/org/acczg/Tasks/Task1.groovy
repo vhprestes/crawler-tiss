79c8f64120e91b468c7c5da59cc45e2baef93455
vhprestes <vhprestes@gmail.com>
2024-10-22 11:18:14 -0300

    refactor: update Task1 download method

--- src/main/groovy/org/acczg/Tasks/Task1.groovy
+++ src/main/groovy/org/acczg/Tasks/Task1.groovy
@@ -2,6 +2,9 @@ package org.acczg.Tasks
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import static groovyx.net.http.HttpBuilder.configure
+import groovyx.net.http.optional.Download
+
 
 class Task1 {
 
@@ -22,9 +25,18 @@ class Task1 {
 
                 String downloadLink = doc.select(".table > tbody:nth-child(2) > tr:nth-child(5) > td:nth-child(3) > a:nth-child(1)").attr("href")
 
-                def fileResponse = Jsoup.connect(downloadLink).ignoreContentType(true).execute()
-                new File("../../downloads/arquivo_padrao_tiss.zip").withOutputStream { it << fileResponse.bodyStream() }
-                print("Arquivo baixado com sucesso")
+//                def fileResponse = Jsoup.connect(downloadLink).ignoreContentType(true).execute()
+//                new File("../../downloads/arquivo_padrao_tiss.zip").withOutputStream { it << fileResponse.bodyStream() }
+//                print("Arquivo baixado com sucesso")
+
+                File getFile = new File("../../downloads/arquivo_padrao_tiss.zip")
+                configure {
+                    request.uri = downloadLink
+                }.get {
+                    Download.toFile(delegate, getFile)
+                    println "Arquivo baixado com sucesso"
+                }
+
             }
             catch (Exception e) {
                 println "Erro ao baixar arquivo padrão TISS" + e
